3b2e0e5b6377e8855916224d085b792ae851eba1
raunaq-foridi <57813440+raunaq-foridi@users.noreply.github.com>
2026-09-05 15:48:07 +0100

    Rendering optimisations
    Hugely improved rendering by using the cached tiles instead of re-acquiring tiles multiple times per pixel
    Fixed Keyboard inputs and event handling
    basic ROMs are now usable.

--- main.py
+++ main.py
@@ -45,63 +45,15 @@ gpu.reset()  # dummy GPU will just log writes
 gpu.wb(0xFF40, 0x91)
 
 # Load a ROM
+mmu.load("Tetris.gb")
 #mmu.load("pkmn_red.gb")
 #mmu.load("cpu_instrs.gb")
-mmu.load("instr_timing.gb")
+#mmu.load("instr_timing.gb")
+#mmu.load("mem_timing.gb")
+#mmu.load("halt_bug.gb")
 
 
 #Functions
-
-'''def frame():
-    fclock = CPU._clock + 17556
-    #brk = document.getElementById('breakpoint').value  # Keep as string, parse later
-    #t0 = datetime.datetime.now()
-    
-    while CPU._clock < fclock:
-        if CPU._halt:
-            CPU.M = 1
-        else:
-            # Execute instruction at PC
-            CPU._map[mmu.rb(CPU.PC)]()
-            CPU.PC = (CPU.PC + 1) & 0xFFFF
-        
-        # Handle interrupts
-        if CPU.T and mmu._ie and mmu._if:
-            CPU._halt = 0
-            CPU.T = 0
-            ifired = mmu._ie & mmu._if
-            
-            if ifired & 1:
-                mmu._if &= 0xFE
-                CPU._ops.RST40()
-            elif ifired & 2:
-                mmu._if &= 0xFD
-                CPU._ops.RST48()
-            elif ifired & 4:
-                mmu._if &= 0xFB
-                CPU._ops.RST50()
-            elif ifired & 8:
-                mmu._if &= 0xF7
-                CPU._ops.RST58()
-            elif ifired & 16:
-                mmu._if &= 0xEF
-                CPU._ops.RST60()
-            else:
-                CPU.T = 1
-        
-        # Update CPU clock, GPU, and timers
-        CPU._clock += CPU.M
-        gpu.checkline()
-        timer.inc()
-        
-        # Breakpoint or stop
-        if CPU._stop:
-            break
-    
-    #t1 = datetime.datetime.now()
-    #elapsed_ms = (t1 - t0).total_seconds() * 1000
-    #fps = round(10000 / elapsed_ms, 1)'''
-
 def frame():
 
     fclock = CPU._clock + 17556
@@ -163,9 +115,7 @@ def frame():
             break 
 
     #t1 = datetime.datetime.now()
-
     #elapsed_ms = (t1 - t0).total_seconds() * 1000
-
     #fps = round(10000 / elapsed_ms, 1)
 
 
@@ -176,12 +126,19 @@ running = True
 CPU.PC=0x100
 print("Reading memory address: ",CPU.PC)
 print("Which should be instruction: ", hex(mmu.rb(CPU.PC)))
-#import pygame
+import pygame
 while running:
    
     
     #input("Press enter to simulate a single frame x 1000: ")
     #for i in range(0,1000):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            key.keydown(event.key)
+        elif event.type == pygame.KEYUP:
+            key.keyup(event.key)
     frame()             
     '''CPU._map[mmu.rb(CPU.PC)]()
     CPU.PC = (CPU.PC + 1) & 0xFFFF
